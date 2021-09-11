33123d9ce59d09d1345ce50586651072926c2e48
Sanujakumari <89187527+Sanujakumari@users.noreply.github.com>
2021-09-11 20:59:27 +0530

    First commit for B27R

---
+++ script.js
@@ -0,0 +1,19 @@
+var  request= new XMLHttpRequest();
+
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+
+
+request.send();
+
+request.onload=function(){
+var data=JSON.parse(request.response);
+console.log(data);
+
+for(var i in data)
+{
+    console.log(data[i].name+" "+data[i].flag+" "+data[i].capital)
+}
+
+
+
+}
